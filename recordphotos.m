756d3b2520d6f5466dff68c2ae8dfea0b380cccd
Demo748 <40484308+Demo748@users.noreply.github.com>
2023-10-24 20:29:28 +1100

    changed image path

--- recordphotos.m
+++ recordphotos.m
@@ -5,6 +5,6 @@ pause(2);
 for i = 1:10
     image_data = readImage(cameraRgbSub.LatestMessage);
     image_matrix = [image_matrix;{image_data}];
-    imwrite(image_matrix{i},strcat('images/outputFront/image',num2str(i),'.png'));
+    imwrite(image_matrix{i},strcat('images/test/image',num2str(i),'.png'));
     pause(3);
 end
