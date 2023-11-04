4200989a3ad5ee2c289f756f5d8c0563635cb7da
Demo748 <40484308+Demo748@users.noreply.github.com>
2023-11-04 15:43:32 +1100

    visualised checkerboard images

--- cameraParams.m
+++ cameraParams.m
@@ -1,4 +1,4 @@
-images = imageDatastore("patternEffector/");
+images = imageDatastore("demo/cameraEffector/");
 imageFiles = images.Files;
 
 % Detect checkerboard points
@@ -13,7 +13,7 @@ imageSize = [size(I,1) size(I,2)];
 params = estimateCameraParameters(imagePoints,worldPoints, ...
                                   'ImageSize',imageSize);
 %%
-showReprojectionErrors(params);
+% showReprojectionErrors(params);
 
 %%
 for i = 1:size(imageFiles)
@@ -22,8 +22,10 @@ for i = 1:size(imageFiles)
     image_h = imshow(imageFiles{i});
     [imagePoints,boardSize] = detectCheckerboardPoints(image_h.CData);
     J = insertText(image_h.CData,imagePoints,1:size(imagePoints,1));
+    % J = insertMarker(J,imagePoints,'o','color','red','Size',5); 
     J = insertMarker(J,imagePoints,'o','MarkerColor','red','Size',5);
     % imshow(J);
     image_h.CData = J;
     title(sprintf('Detected a %d x %d Checkerboard',boardSize));
+    % saveas(gcf,[strcat('demo/cameraEffector/imagePoints',num2str(i),'.png')]);
 end
