5e638baaf5bf2791a6817eac639b2738c19d972e
Demo748 <40484308+Demo748@users.noreply.github.com>
2023-11-04 17:39:52 +1100

    visualised camera extrinsics, camera pose

--- cameraParams.m
+++ cameraParams.m
@@ -1,4 +1,4 @@
-images = imageDatastore("demo/cameraEffector/");
+images = imageDatastore("demo/patternEffector/");
 imageFiles = images.Files;
 
 % Detect checkerboard points
@@ -13,7 +13,17 @@ imageSize = [size(I,1) size(I,2)];
 params = estimateCameraParameters(imagePoints,worldPoints, ...
                                   'ImageSize',imageSize);
 %%
+% visualise
 % showReprojectionErrors(params);
+showExtrinsics(params,'CameraCentric')
+% saveas(gcf,'demo/cameraEffector/visualised/showExtrinsics.png');
+% plotCamera
+%%
+% estimate pose
+% ext = estimateExtrinsics
+% intrinsics = params.Intrinsics;
+% camExtrinsics = estimateExtrinsics(imagePoints,worldPoints,intrinsics)
+camPose1 = extr2pose(params.PatternExtrinsics(1));
 
 %%
 for i = 1:size(imageFiles)
@@ -22,10 +32,11 @@ for i = 1:size(imageFiles)
     image_h = imshow(imageFiles{i});
     [imagePoints,boardSize] = detectCheckerboardPoints(image_h.CData);
     J = insertText(image_h.CData,imagePoints,1:size(imagePoints,1));
-    % J = insertMarker(J,imagePoints,'o','color','red','Size',5); 
-    J = insertMarker(J,imagePoints,'o','MarkerColor','red','Size',5);
+    J = insertMarker(J,imagePoints,'o','color','red','Size',5);
+    % J = insertMarker(J,imagePoints,'o','MarkerColor','red','Size',5);
     % imshow(J);
     image_h.CData = J;
     title(sprintf('Detected a %d x %d Checkerboard',boardSize));
+    % exportgraphics(gcf,[strcat('demo/patternEffector/visualised/imagePoints',num2str(i),'.png')]);
     % saveas(gcf,[strcat('demo/cameraEffector/imagePoints',num2str(i),'.png')]);
 end
